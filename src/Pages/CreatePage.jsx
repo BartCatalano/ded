85078a38addb99be4065d4022f4d29328435cb55
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-06 19:20:32 +0100

    feat: inizio pagina creazione

--- src/Pages/CreatePage.jsx
+++ src/Pages/CreatePage.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
+import style from "./CreatePage.module.css";
 // scheda personaggio vuota
-const Player ={
-nome:"",
-razza:"",
-classe:"",
-statistiche:{
-    forza:0,
-    destrezza:0,
-    costituzione:0,
-    intelligenza:0,
-    saggezza:0,
-    carisma:0
-},
-caratteristiche:{
-    altezza:0,
-    peso:0,
-    background:"",
-    
-}};
+const Player = {
+  nome: "",
+  razza: "",
+  classe: "",
+  sesso: "",
+  statistiche: {
+    forza: 0,
+    destrezza: 0,
+    costituzione: 0,
+    intelligenza: 0,
+    saggezza: 0,
+    carisma: 0
+  },
+  caratteristiche: {
+    altezza: 0,
+    peso: 0,
+    background: "",
+
+  }
+};
 // creo ogetto con bonus di classe
 const classBonuses = {
   guerriero: { forza: 3 },
@@ -29,144 +32,173 @@ const classBonuses = {
 };
 // creo il bonus per la razza scelta
 const razzaBonuses = {
-  umano: { carisma: 1, forza: 1, destrezza: 1, intelligenza: 1, saggezza: 1, altezza: 120,peso: 50 },
-  elfo: { destrezza: 2, intelligenza: 1, saggezza: 1,altezza: 150,peso: 40 },
+  umano: { carisma: 1, forza: 1, destrezza: 1, intelligenza: 1, saggezza: 1, altezza: 120, peso: 50 },
+  elfo: { destrezza: 2, intelligenza: 1, saggezza: 1, altezza: 150, peso: 40 },
   nano: { costituzione: 2, forza: 2, altezza: 50, peso: 50 },
-  mezzelfo: { carisma: 2, intelligenza: 1, saggezza: 1,altezza: 150,peso: 40 },
-  mezzorco: { forza: 2, costituzione: 1, altezza: 140, peso: 50  },
-  gnomo: { intelligenza: 2, destrezza: 1, carisma: 2, altezza: 40,peso: 25 },
+  mezzelfo: { carisma: 2, intelligenza: 1, saggezza: 1, altezza: 150, peso: 40 },
+  mezzorco: { forza: 2, costituzione: 1, altezza: 140, peso: 50 },
+  gnomo: { intelligenza: 2, destrezza: 1, carisma: 2, altezza: 40, peso: 25 },
 };
 
 function CreatePage() {
 
-const [nameHero,setNameHero] = useState("");
-const [classe,setClasse] = useState("");
-const [caratteristiche,setcaratteristiche] = useState({});
-const [razza,setRazza] = useState();
-const [newPlayer,setNewPlayer] = useState(Player);
+  const [nameHero, setNameHero] = useState("");
+  const [classe, setClasse] = useState("");
+  const [caratteristiche, setcaratteristiche] = useState({});
+  const [razza, setRazza] = useState();
+  const [newPlayer, setNewPlayer] = useState(Player);
+  const [sesso, setSesso] = useState("");
+  const [isCharacterCreated, setIsCharacterCreated] = useState(false);
 
 
-
-const createNewPlayer = () => {
-  // prendo i bonus della classe scelta
-  const bonus = classBonuses[classe] || {};
-  const razzaBonus = razzaBonuses[razza] || {};
+  const createNewPlayer = () => {
+    // prendo i bonus della classe scelta
+    const bonus = classBonuses[classe] || {};
+    const razzaBonus = razzaBonuses[razza] || {};
     setNewPlayer({
-        nome:nameHero,
-        razza:razza,
-        classe:classe,
-        statistiche:{
-          forza: (Math.floor(Math.random() * 6) + 1) + (bonus.forza || 0) + (razzaBonus.forza || 0),
-          destrezza: (Math.floor(Math.random() * 6) + 1) + (bonus.destrezza || 0) + (razzaBonus.destrezza || 0),
-          costituzione: (Math.floor(Math.random() * 6) + 1) + (bonus.costituzione || 0) + (razzaBonus.costituzione || 0),
-          intelligenza: (Math.floor(Math.random() * 6) + 1) + (bonus.intelligenza || 0) + (razzaBonus.intelligenza || 0),
-          saggezza: (Math.floor(Math.random() * 6) + 1) + (bonus.saggezza || 0) + (razzaBonus.saggezza || 0),
-          carisma: (Math.floor(Math.random() * 6) + 1) + (bonus.carisma || 0) + (razzaBonus.carisma || 0),
-        },
-        caratteristiche:{
-            allineamento:caratteristiche,
-            altezza:Math.floor(Math.random()*100) + razzaBonus.altezza, 
-            peso:Math.floor(Math.random()*100) + razzaBonus.peso, 
-        }
+      nome: nameHero,
+      sesso: sesso,
+      razza: razza,
+      classe: classe,
+      statistiche: {
+        forza: (Math.floor(Math.random() * 6) + 1) + (bonus.forza || 0) + (razzaBonus.forza || 0),
+        destrezza: (Math.floor(Math.random() * 6) + 1) + (bonus.destrezza || 0) + (razzaBonus.destrezza || 0),
+        costituzione: (Math.floor(Math.random() * 6) + 1) + (bonus.costituzione || 0) + (razzaBonus.costituzione || 0),
+        intelligenza: (Math.floor(Math.random() * 6) + 1) + (bonus.intelligenza || 0) + (razzaBonus.intelligenza || 0),
+        saggezza: (Math.floor(Math.random() * 6) + 1) + (bonus.saggezza || 0) + (razzaBonus.saggezza || 0),
+        carisma: (Math.floor(Math.random() * 6) + 1) + (bonus.carisma || 0) + (razzaBonus.carisma || 0),
+      },
+      caratteristiche: {
+        allineamento: caratteristiche,
+        altezza: Math.floor(Math.random() * 100) + razzaBonus.altezza,
+        peso: Math.floor(Math.random() * 100) + razzaBonus.peso,
+      }
     });
-}
+    setIsCharacterCreated(true);
+  }
 
 
 
-// creo la funzione di salvataggio nome
-const creazioneNome = (e) => {
-    setNameHero(e.target.value);}
-// funzione per la selezioneClasse
-const selezioneClasse = (e) => {
-    setClasse(e.target.value);}
-// funzione per la creazione delle caratteristiche
-const creazioneCaratteristiche = (e) => {
-    setcaratteristiche(e.target.value);}
-// funzione per la selezione della razza
-    const selezioneRazza = (e) => {
-        setRazza(e.target.value);}
-
+  // creo la funzione di salvataggio nome
+  const creazioneNome = (e) => {
+    setNameHero(e.target.value);
+  }
+  // funzione per la selezioneClasse
+  const selezioneClasse = (e) => {
+    setClasse(e.target.value);
+  }
+  // funzione per la creazione delle caratteristiche
+  const creazioneCaratteristiche = (e) => {
+    setcaratteristiche(e.target.value);
+  }
+  // funzione per la selezione della razza
+  const selezioneRazza = (e) => {
+    setRazza(e.target.value);
+  }
+  // funzione per il sesso
+  const selezioneSesso = (e) => {
+    setSesso(e.target.value);
+  }
 
   return (
     <>
-    <div className="container">
-      <h1>Create Page</h1>
-{/* inserisco l'input di nome e la selezione di razza classe allineamento */}
-{/* nome eroe */}
-<label htmlFor="Hero">Nome Eroe:</label>
-<input type="text" value={nameHero} onChange={creazioneNome} name="Hero" id="Hero" />
+      <div className={`${style.body}`}>
+        <div className={`${style.bodyCreate}`}>
+          {/* form creativo */}
+          <section className={`${style.formCreazione}`}>
+            <h1>Crea Il tuo Eroe</h1>
+            {/* inserisco l'input di nome e la selezione di razza classe allineamento */}
+            {/* nome eroe */}
+            <label htmlFor="Hero">Nome Eroe:</label>
+            <input type="text" value={nameHero} onChange={creazioneNome} name="Hero" id="Hero" />
 
-{/* inserimento caratteristiche */}
-{/* selezione razza */}
-<div >
+            {/* inserimento caratteristiche */}
+            {/* selezione razza */}
+            <div >
 
-      <label htmlFor="dropdown">
-        Scegli La tua razza:
-      </label>
-      <select
-        id="dropdown"
-        value={razza}
-        onChange={selezioneRazza}
-      >
-        <option value="">Seleziona...</option>
-        <option value="umano">Umano</option>
-        <option value="elfo">Elfo</option>
-        <option value="nano">Nano</option>
-        <option value="mezzelfo">Mezzelfo</option>
-        <option value="mezzorco">Mezzorco</option>
-        <option value="gnomo">Gnomo</option>
-      </select>
+              <label htmlFor="dropdown">
+                Scegli La tua razza:
+              </label>
+              <select
+                id="dropdown"
+                value={razza}
+                onChange={selezioneRazza}
+              >
+                <option value="">Seleziona...</option>
+                <option value="umano">Umano</option>
+                <option value="elfo">Elfo</option>
+                <option value="nano">Nano</option>
+                <option value="mezzelfo">Mezzelfo</option>
+                <option value="mezzorco">Mezzorco</option>
+                <option value="gnomo">Gnomo</option>
+              </select>
 
-    </div>
-{/* selezione classe */}
-<div >
+            </div>
+            {/* selezione sesso */}
+            <div>
+              <label htmlFor="dropdown">
+                Scegli il sesso del tuo eroe:
+              </label>
+              <select
+                id="dropdown"
+                value={sesso}
+                onChange={selezioneSesso}
+              >
+                <option value="">Seleziona...</option>
+                <option value="Uomo">Uomo</option>
+                <option value="Donna">Donna</option>
+              </select>
+            </div>
+            {/* selezione classe */}
+            <div >
 
-      <label htmlFor="dropdown">
-        Scegli La tua classe:
-      </label>
-      <select
-        id="dropdown"
-        value={classe}
-        onChange={selezioneClasse}
-      >
-        <option value="">Seleziona...</option>
-        <option value="guerriero">Guerriero</option>
-        <option value="mago">Mago</option>
-        <option value="ladro">Ladro</option>
-        <option value="bardo">Bardo</option>
-        <option value="chierico">Chierico</option>
-        <option value="druido">Druido</option>
-      </select>
-      {/* inserimento allineamento */}
-     <div>
-     <label htmlFor="dropdown">
-        Scegli il tuo allineamento:
-        </label>
-        <select
-            id="allineamento"
-            value={caratteristiche.allineamento}
-            onChange={creazioneCaratteristiche}
-        >
-            <option value="">Seleziona...</option>
-            <option value="legale buono">Legale Buono</option>
-            <option value="neutrale buono">Neutrale Buono</option>
-            <option value="caotico buono">Caotico Buono</option>
-            <option value="legale neutrale">Legale Neutrale</option>
-            <option value="neutrale puro">Neutrale Puro</option>
-            <option value="caotico neutrale">Caotico Neutrale</option>
-            <option value="legale malvagio">Legale Malvagio</option>
-            <option value="neutrale malvagio">Neutrale Malvagio</option>
-            <option value="caotico malvagio">Caotico Malvagio</option>
-        </select>
-     </div>
-    </div>
-     {/* creazione del personaggio */}
-    <div>
-        <button onClick={createNewPlayer}>Crea il Tuo Eroe</button>
-        <div>
+              <label htmlFor="dropdown">
+                Scegli La tua classe:
+              </label>
+              <select
+                id="dropdown"
+                value={classe}
+                onChange={selezioneClasse}
+              >
+                <option value="">Seleziona...</option>
+                <option value="guerriero">Guerriero</option>
+                <option value="mago">Mago</option>
+                <option value="ladro">Ladro</option>
+                <option value="bardo">Bardo</option>
+                <option value="chierico">Chierico</option>
+                <option value="druido">Druido</option>
+              </select>
+              {/* inserimento allineamento */}
+              <div>
+                <label htmlFor="dropdown">
+                  Scegli il tuo allineamento:
+                </label>
+                <select
+                  id="allineamento"
+                  value={caratteristiche.allineamento}
+                  onChange={creazioneCaratteristiche}
+                >
+                  <option value="">Seleziona...</option>
+                  <option value="legale buono">Legale Buono</option>
+                  <option value="neutrale buono">Neutrale Buono</option>
+                  <option value="caotico buono">Caotico Buono</option>
+                  <option value="legale neutrale">Legale Neutrale</option>
+                  <option value="neutrale puro">Neutrale Puro</option>
+                  <option value="caotico neutrale">Caotico Neutrale</option>
+                  <option value="legale malvagio">Legale Malvagio</option>
+                  <option value="neutrale malvagio">Neutrale Malvagio</option>
+                  <option value="caotico malvagio">Caotico Malvagio</option>
+                </select>
+              </div>
+            </div>
+            <button onClick={createNewPlayer}>Crea il Tuo Eroe</button>
+          </section>
+          {/* creazione del personaggio */}
+          <div className={isCharacterCreated ? style.statsVisible : style.statsHidden}>
             <h2>{newPlayer.nome}</h2>
             <h3>{newPlayer.razza}</h3>
             <h3>{newPlayer.classe}</h3>
+            <h3>{newPlayer.sesso}</h3>
             <h3>Statistiche</h3>
             <p>Forza: {newPlayer.statistiche.forza}</p>
             <p>Destrezza: {newPlayer.statistiche.destrezza}</p>
@@ -178,13 +210,13 @@ const creazioneCaratteristiche = (e) => {
             <p>Allineamento: {newPlayer.caratteristiche.allineamento}</p>
             <p>Altezza: {newPlayer.caratteristiche.altezza}</p>
             <p>Peso: {newPlayer.caratteristiche.peso}</p>
-            </div>
-    </div>
-    </div>
-        </>
+
+          </div>
+        </div></div>
+    </>
   );
 }
 export default CreatePage;
 
 
-  
+
